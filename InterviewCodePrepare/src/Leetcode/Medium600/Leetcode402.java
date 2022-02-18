0c94b7808b60829bafcdd614e6f5c9e73d2b3b91
alconzy <alangb013@gmail.com>
2022-02-18 12:35:50 -0500

    Leetcode402 update v0

---
+++ InterviewCodePrepare/src/Leetcode/Medium600/Leetcode402.java
@@ -0,0 +1,21 @@
+package Leetcode.Medium600;
+
+public class Leetcode402 {
+    public static void main(String[] args) {
+        /**
+         * 1.
+         * todo 罗列各种转换String or Character to Integer 的方式
+         * Character.getNumericValue(char element);
+         *
+         * Integer.valueOf(String str): return a Integer object;
+         *
+         * Integer.parseInt(String str): return primitive int value;
+         *
+         *
+         * 2. todo
+         *      一开始思路想要把character 转化为 int 才能方便比较，但是没有意识到 char 可以直接比较，
+         *      原因，ASCII 中 '3' < '4' < '5' < ..... '1009' < ....
+         */
+
+    }
+}
